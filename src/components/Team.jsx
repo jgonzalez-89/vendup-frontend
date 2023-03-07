6a765002c4da39634f2535a69589039af543dde3
jgonzalez-89 <kbcerverjo@gmail.com>
2023-03-07 18:34:35 +0100

    fix erros

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
+import LogoVendup from '../public/vendup.png';
+
 // import Navbar from './Navbar.jsx';
 import '../style/team.css';
 import jose from '../public/Jose.png';
-import quique from'../public/quique.jpg';
-import lola from'../public/Lola.png';
+import quique from '../public/quique.jpg';
+import lola from '../public/Lola.png';
 import Header from './Navbar.jsx';
 
 const Team = () => (
   <>
-    <Header NavHome={'/'} NavProducts={'/products'} NavUser={'/user'} />
-
-    <hr className="featurette-divider"/>
+    <Navbar bg="dark" variant="dark" style={{ minHeight: '50px', top: '0' }}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={LogoVendup} width="100" role="img" aria-label="Vendup" />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
 
     <div className="cuerpopagina row gx-5">
       <div className="col-lg-4">
@@ -18,7 +25,7 @@ const Team = () => (
           <div className="box">
             <div className="body">
               <div className="imgContainer">
-                <img src={jose} alt=""/>
+                <img src={jose} alt="" />
               </div>
               <div className="content d-flex flex-column justify-content-center">
                 <div className="center">
@@ -26,7 +33,14 @@ const Team = () => (
                   <p className="fs-6 text-white">FullStack Developer</p>
                   <p>
                     <a className="btn btn-dark" href="https://www.linkedin.com/in/jgonzalez89/">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-linkedin"
+                        viewBox="0 0 16 16"
+                      >
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                       </svg>
                     </a>
@@ -48,7 +62,7 @@ const Team = () => (
           <div className="box">
             <div className="body">
               <div className="imgContainer">
-              <img src={quique} alt=""/>
+                <img src={quique} alt="" />
               </div>
               <div className="content d-flex flex-column justify-content-center">
                 <div className="center">
@@ -56,7 +70,14 @@ const Team = () => (
                   <p className="fs-6 text-white">Frontend Developed</p>
                   <p>
                     <a className="btn btn-dark" href="https://www.linkedin.com/in/j-enrique-ar%C3%A9s-lorenzo-6a636a148/">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-linkedin"
+                        viewBox="0 0 16 16"
+                      >
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                       </svg>
                     </a>
@@ -78,7 +99,7 @@ const Team = () => (
           <div className="box">
             <div className="body">
               <div className="imgContainer">
-                <img src={lola} alt=""/>
+                <img src={lola} alt="" />
               </div>
               <div className="content d-flex flex-column justify-content-center">
                 <div className="center">
@@ -86,7 +107,14 @@ const Team = () => (
                   <p className="fs-6 text-white">Frontend Developed</p>
                   <p>
                     <a className="btn btn-dark" href="https://www.linkedin.com/in/lola-petrement/">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" className="bi bi-linkedin" viewBox="0 0 16 16">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="20"
+                        height="20"
+                        fill="currentColor"
+                        className="bi bi-linkedin"
+                        viewBox="0 0 16 16"
+                      >
                         <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z" />
                       </svg>
                     </a>
--- src/components/Terms.jsx
+++ src/components/Terms.jsx
@@ -3,12 +3,18 @@ import Card from 'react-bootstrap/Card';
 import Header from './Navbar.jsx';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Footer from './Footer.jsx';
-
-// import Navbar from '../component/Navbar.jsx';
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
+import LogoVendup from '../public/vendup.png';
 
 const Terms = () => (
   <>
-    <Header NavHome={'/'} NavProducts={'/products'} NavUser={'/user'} />
+    <Navbar bg="dark" variant="dark" style={{ minHeight: '50px', top: '0' }}>
+      <Container>
+        <Navbar.Brand href="/">
+          <img src={LogoVendup} width="100" role="img" aria-label="Vendup" />
+        </Navbar.Brand>
+      </Container>
+    </Navbar>
     <br />
     <Card>
       <Card.Header>
@@ -23,49 +29,54 @@ const Terms = () => (
 
         <ListGroup variant="flush">
           <ListGroup.Item>
-            Objeto: Vendup es una tienda en línea que vende productos a través de Internet. El objetivo de estos términos y condiciones es establecer las condiciones generales de
-            uso del sitio web y las condiciones de compra de los productos ofrecidos.
-          </ListGroup.Item>
-          <ListGroup.Item>Aceptación de los términos: Al utilizar el sitio web de Vendup, el usuario acepta estos términos y condiciones.</ListGroup.Item>
-          <ListGroup.Item>
-            Registro: Para realizar una compra en Vendup, el usuario debe registrarse en el sitio web proporcionando su nombre, dirección de correo electrónico y otra información
-            personal relevante. El usuario se compromete a proporcionar información precisa y actualizada.
+            Objeto: Vendup es una tienda en línea que vende productos a través de Internet. El objetivo de estos términos y condiciones es establecer
+            las condiciones generales de uso del sitio web y las condiciones de compra de los productos ofrecidos.
           </ListGroup.Item>
           <ListGroup.Item>
-            Precios: Los precios de los productos en Vendup están sujetos a cambios sin previo aviso. Los precios que aparecen en el sitio web en el momento de la compra son los
-            precios que se aplican.
+            Aceptación de los términos: Al utilizar el sitio web de Vendup, el usuario acepta estos términos y condiciones.
           </ListGroup.Item>
           <ListGroup.Item>
-            Métodos de pago: Vendup acepta varios métodos de pago, como tarjetas de crédito, débito y PayPal. El usuario debe proporcionar información precisa y actualizada de su
-            método de pago.
+            Registro: Para realizar una compra en Vendup, el usuario debe registrarse en el sitio web proporcionando su nombre, dirección de correo
+            electrónico y otra información personal relevante. El usuario se compromete a proporcionar información precisa y actualizada.
           </ListGroup.Item>
           <ListGroup.Item>
-            Entrega: Vendup enviará los productos al usuario en el plazo establecido en la página de detalles de cada producto. El plazo de entrega puede variar dependiendo de la
-            ubicación del usuario y de otros factores externos. El usuario es responsable de proporcionar una dirección de entrega precisa.
+            Precios: Los precios de los productos en Vendup están sujetos a cambios sin previo aviso. Los precios que aparecen en el sitio web en el
+            momento de la compra son los precios que se aplican.
           </ListGroup.Item>
           <ListGroup.Item>
-            Devoluciones: El usuario puede devolver los productos dentro de los 14 días siguientes a la recepción del pedido, siempre y cuando estén en su estado original y sin
-            usar. Vendup se reserva el derecho de rechazar cualquier devolución que no cumpla con estos requisitos.
+            Métodos de pago: Vendup acepta varios métodos de pago, como tarjetas de crédito, débito y PayPal. El usuario debe proporcionar información
+            precisa y actualizada de su método de pago.
           </ListGroup.Item>
           <ListGroup.Item>
-            Política de privacidad: Vendup respeta la privacidad del usuario y protege sus datos personales de acuerdo con las leyes de protección de datos aplicables. El usuario
-            puede leer la política de privacidad completa en el sitio web de Vendup.
+            Entrega: Vendup enviará los productos al usuario en el plazo establecido en la página de detalles de cada producto. El plazo de entrega
+            puede variar dependiendo de la ubicación del usuario y de otros factores externos. El usuario es responsable de proporcionar una dirección
+            de entrega precisa.
           </ListGroup.Item>
           <ListGroup.Item>
-            Propiedad intelectual: Todo el contenido del sitio web de Vendup, incluyendo textos, imágenes y logos, está protegido por derechos de propiedad intelectual y no puede
-            ser utilizado sin el permiso expreso de Vendup.
+            Devoluciones: El usuario puede devolver los productos dentro de los 14 días siguientes a la recepción del pedido, siempre y cuando estén
+            en su estado original y sin usar. Vendup se reserva el derecho de rechazar cualquier devolución que no cumpla con estos requisitos.
           </ListGroup.Item>
           <ListGroup.Item>
-            Limitación de responsabilidad: Vendup no será responsable de los daños directos o indirectos causados al usuario o a terceros como resultado del uso del sitio web o de
-            la compra de productos en el sitio web.
+            Política de privacidad: Vendup respeta la privacidad del usuario y protege sus datos personales de acuerdo con las leyes de protección de
+            datos aplicables. El usuario puede leer la política de privacidad completa en el sitio web de Vendup.
           </ListGroup.Item>
           <ListGroup.Item>
-            Legislación aplicable y jurisdicción: Estos términos y condiciones se regirán e interpretarán de acuerdo con las leyes del país donde Vendup está ubicada. Cualquier
-            disputa relacionada con estos términos y condiciones será resuelta por los tribunales competentes de esa jurisdicción.
+            Propiedad intelectual: Todo el contenido del sitio web de Vendup, incluyendo textos, imágenes y logos, está protegido por derechos de
+            propiedad intelectual y no puede ser utilizado sin el permiso expreso de Vendup.
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Limitación de responsabilidad: Vendup no será responsable de los daños directos o indirectos causados al usuario o a terceros como
+            resultado del uso del sitio web o de la compra de productos en el sitio web.
+          </ListGroup.Item>
+          <ListGroup.Item>
+            Legislación aplicable y jurisdicción: Estos términos y condiciones se regirán e interpretarán de acuerdo con las leyes del país donde
+            Vendup está ubicada. Cualquier disputa relacionada con estos términos y condiciones será resuelta por los tribunales competentes de esa
+            jurisdicción.
           </ListGroup.Item>
         </ListGroup>
         <Card.Footer className="text-white text-center">
-          Es importante que los usuarios lean y comprendan estos términos y condiciones antes de utilizar el sitio web o realizar una compra en Vendup.
+          Es importante que los usuarios lean y comprendan estos términos y condiciones antes de utilizar el sitio web o realizar una compra en
+          Vendup.
         </Card.Footer>
       </Card.Body>
     </Card>
