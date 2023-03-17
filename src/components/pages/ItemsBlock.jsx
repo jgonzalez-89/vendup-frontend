86194ff2e2efa6b8b62a8820c1ae16ff4bd043cc
jgonzalez-89 <kbcerverjo@gmail.com>
2023-03-17 16:17:26 +0100

    Integrate LandingPage

---
+++ src/components/pages/ItemsBlock.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { HttpHandler } from '../../http/handler';
+import CardFree from '../../components/CardFree.jsx';
+import CardPremium from '../../components/CardPremium.jsx';
+
+function ItemsBlock() {
+  const [itemsPerPage, setItemsPerPage] = useState(3); // Cambiar aquí el valor de 9 a 3
+  const [category, setCategory] = useState('');
+  const [searchText, setSearchText] = useState('');
+  const [data, setData] = useState({});
+  const [page, setPage] = useState(1);
+
+  const handler = new HttpHandler();
+
+  useEffect(() => {
+    async function fetchData() {
+      const result = await handler.getProduct(); // eliminar el parámetro sort
+      setData(result);
+    }
+    fetchData();
+  }, []);
+
+  let filteredItems = [];
+
+  if (data.product) {
+    filteredItems = data.product.filter(
+      (item) =>
+        item.status_shooping &&
+        item.premium === true &&
+        (category === '' || item.category === category) &&
+        (searchText === '' || item.name.toLowerCase().includes(searchText.toLowerCase()))
+    );
+  }
+
+  const startIndex = (page - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const currentPageItems = filteredItems.slice(startIndex, endIndex);
+
+  return (
+    <>
+      <div className="container">
+        <div className="row">
+          {currentPageItems.map((item, index) => {
+            const Component = item.premium ? CardPremium : CardFree;
+
+            return (
+              <div className="col-lg-4 col-md-6 col-12 my-1 mb-5" key={index}>
+                <Component item={item} image={item.images} title={item.name} description={item.description} price={item.price} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ItemsBlock;
--- src/components/pages/PlataformBlock.jsx
+++ src/components/pages/PlataformBlock.jsx
@@ -57,21 +57,3 @@ const PlataformBlock = ({ title, content }) => {
 };
 
 export default PlataformBlock;
-
-{
-  /* <Container className="mt-5 text-center">
-<Row>
-  <Col xs={12} md={6} className="col-12 col-md-6 order-md-1">
-    <img src={src} alt="imagen..." />
-  </Col>
-  <Col xs={12} md={6} className="order-md-2">
-    <Fade right>
-      <div className="d-flex justify-content-center align-items-center flex-column h-100">
-        <h1 className="display-5">{title}</h1>
-        <p>{content}</p>
-      </div>
-    </Fade>
-  </Col>
-</Row>
-</Container>  */
-}
